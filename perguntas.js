d33c7d8a72bf0d7981faa1bb40896b8a9719b06a
marcosgabrielgreco <165661707+marcosgabrielgreco@users.noreply.github.com>
2025-08-28 10:53:39 -0300

    s

--- perguntas.js
+++ perguntas.js
@@ -21,7 +21,7 @@ criaCartao(
 criaCartao(
 'Historia',
 'Quem foi luiz 16?',
-'Luís XVI (1754-1793) foi o último rei da França antes da Revolução Francesa, que o depôs em 1792 e o guilhotinou em 1793 por traição, sendo lembrado como um monarca que falhou em modernizar o país e se opôs às reformas revolucionárias. Sua execução em 21 de janeiro de 1793 marcou o fim da monarquia e o início da fase mais violenta da Revolução, conhecida como o Período do Terror. .'
+'Luís XVI (1754-1793) foi o último rei da França antes da Revolução Francesa, que o depôs em 1792 e o guilhotinou em 1793 . .'
 )
 criaCartao(
 'Programação',
